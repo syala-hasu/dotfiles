eb0545f5dabd737c40076636c711e578611806cb
Ekahs <syalahasu@gmail.com>
2020-02-21 17:14:10 +0900

    add bashrc

---
+++ bash/init.sh
@@ -0,0 +1,13 @@
+INIT_DIR=$(cd $(dirname $0); pwd)
+cd $INIT_DIR
+
+ln -s $INIT_DIR/.bashrc ~/.bashrc
+
+grep 'if \[ -f ~/.bashrc \] ; then' ~/.bash_profile > /dev/null 2>&1
+
+if [ $? = 1 ] ; then
+  echo "" >> ~/.bash_profile
+  echo "if [ -f ~/.bashrc ] ; then" >> ~/.bash_profile
+  echo "    . ~/.bashrc" >> ~/.bash_profile
+  echo "fi" >> ~/.bash_profile
+fi
